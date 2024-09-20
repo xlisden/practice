325cff0e1fc5acc28b9bea7b6741cf84d2a47ecc
Lisden <128757464+xlisden@users.noreply.github.com>
2024-09-20 10:28:58 -0500

    Add files via upload

---
+++ GPC-UNI/Arrays_introduction.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int array[n];
+	
+	for(int i=0; i<n; i++){
+		scanf("%d", &array[i]);
+	}
+	
+	for(int i=n-1; i>=0; i--){
+		printf("%d ", array[i]);
+	}
+	return 0;
+}
